27fa2319a63080345a4dd083f5417aba142c01d0
sumitxt <sumitbarua14@gmail.com>
2022-12-30 22:36:12 +0600

    fixed increase,decrease function
    typo in reducers.

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {decrement, increment} from "../../redux/state/counter/CounterSlice";
+import {decrement, increment, setCustom} from "../../redux/state/counter/CounterSlice";
+
 
 const Counter = () => {
+
+    const myNumber=useRef()
 
     const count= useSelector((state)=>
         state.counter.value
     )
-    const dispatch = useDispatch()
-    // const increaser=()=>{dispatch(increment)}
+    const dispatch = useDispatch();
+    // const increaser=()=>{dispatch(increment()}
     //
-    // const decreaser=()=>{dispatch(decrement)}
+    // const decreaser=()=>{dispatch(decrement()}
 
     return (
         <div className="card">
@@ -20,8 +23,12 @@ const Counter = () => {
             <div className="card-body"></div>
             <h1>{count}</h1>
             <div className="my-4">
-                <button onClick={()=>{dispatch(increment)}} className="btn btn-success">Increase</button>
-                <button onClick={()=>{dispatch(decrement)}} className="btn mx-2 btn-danger">Decrease</button>
+                <button onClick={() => dispatch(increment())} className="btn btn-success">Increase</button>
+                <button onClick={()=>{dispatch(decrement())}} className="btn mx-2 btn-danger">Decrease</button>
+            </div>
+            <div className="my-4">
+                <input ref={myNumber} className="form-control w-50 my-2" type="number" />
+                    <button onClick={()=>dispatch(setCustom(myNumber.current.value))} className="btn btn-success">Set Custom</button>
             </div>
         </div>
     );
--- src/redux/state/counter/CounterSlice.js
+++ src/redux/state/counter/CounterSlice.js
@@ -1,19 +1,22 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-export const counterSlice=createSlice({
-        name:"counter",
-        initialState:{
-            value:0
+export const counterSlice = createSlice({
+    name: "counter",
+    initialState: {
+        value: 0
+    },
+    reducers: {
+        increment: (state) => {
+            state.value = state.value+1
         },
-    reducer:{
-            increment:(state)=>{
-                state.value += 1
-            },
-        decrement:(state)=>{
-                state.value -=1
+        decrement: (state) => {
+            state.value = state.value- 1
+        },
+        setCustom: (state, action) => {
+            state.value = action.payload;
         }
     }
 })
 
-export const {increment,decrement}=counterSlice.actions;
+export const { increment, decrement, setCustom } = counterSlice.actions;
 export default counterSlice.reducer;
